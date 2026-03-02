a15e55147de5bfdeb0b7f70c6eace9335585ae21
dougreichard <doug.reichard+github@gmail.com>
2026-03-02 18:29:12 -0500

    Add version file for sbslibs

--- src/lib_cmd.py
+++ src/lib_cmd.py
@@ -47,6 +47,12 @@ def lib_impl(folder, user):
     repo = folder
     for key, values in libs.items():
         if key == "version":
+            v = version_file_contents(values)
+            if v is None:
+                continue
+            version_file = Path(working_directory).resolve() / folder / folder / "version__.py"
+            with open(version_file, "w") as f:
+                f.write(v)
             continue
         ext = key
         for folder_path in values:
@@ -63,8 +69,36 @@ def lib_impl(folder, user):
             
             #print(f"Compressing {lib_dir} into  {zip_file_name}")
 
+
+def version_file_contents(version):
+    if version is None:
+        return None
+    v = version.split(".")
+    print(version)
+    if len(v)!=3:
+        return None
+    v[0] = v[0][1:]
+    return f"""
+__version = ({v[0]},{v[1]},{v[2]})
+def version_get():
+    return __version
+
+def version_get_major():
+    return __version[0]
+
+def version_get_minor():
+    return __version[1]
+
+def version_get_build():
+    return __version[2]
+
+"""
+
+
+
 @cli.command(short_help="Build libraries/addons.")
 @click.argument("folder", default="LegendaryMissions")
 @click.option('-u', '--user', default="artemis-sbs", show_default=True, help="Specify The github user/organization.")
 def lib(folder, user):
     lib_impl(folder, user)
+
